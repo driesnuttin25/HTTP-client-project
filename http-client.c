541dad312934684177986bc87c2fb74ba814aeb9
Dries Nuttin <dries.nuttin@hotmail.com>
2023-05-19 12:15:26 +0200

    tcp-server
    tcp-server can now send attacks to the incoming request and logs the amount of bytes sent. Still needs to keep sending data, now it only sends when it receives a message.

--- http-client.c
+++ http-client.c
@@ -71,7 +71,7 @@ int main( int argc, char * argv[] )
     //Clean up//
     ////////////
 
-    cleanup( internet_socket, client_internet_socket );
+    //cleanup( internet_socket, client_internet_socket );
 
     OSCleanup();
 
@@ -182,7 +182,7 @@ int connection(int internet_socket) {
 }
 
 
-void http_get(const char* ip_address) {
+void http_get() {
     int sockfd;
     struct sockaddr_in server_addr;
     char request[256];
@@ -248,38 +248,60 @@ void http_get(const char* ip_address) {
 }
 
 
-void execution(int internet_socket) {
+void execution(int client_internet_socket) {
     // Step 1: Receive initial data
     printf("Execution Start?!\n");
     char buffer[1000];
-    int number_of_bytes_received = recv(internet_socket, buffer, (sizeof buffer) - 1, 0);
-    if (number_of_bytes_received == -1) {
-        perror("recv");
-    } else {
+    int total_bytes_received = 0;
+    int number_of_bytes_received;
+    http_get();
+
+    while ((number_of_bytes_received = recv(client_internet_socket, buffer, sizeof(buffer) - 1, 0)) > 0) {
         buffer[number_of_bytes_received] = '\0';
         printf("Received: %s\n", buffer);
+
+        // Write the received message in the log file
+        FILE *log_file = fopen("log.txt", "a");
+        if (log_file == NULL) {
+            perror("fopen");
+            close(client_internet_socket);
+            exit(4);
+        }
+        fprintf(log_file, "------------------------\n");
+        fprintf(log_file, "Message from client: %s\n", buffer);
+        fprintf(log_file, "------------------------\n");
+        fclose(log_file);
+
+        // Send the received data back to the client
+        int bytes_sent = send(client_internet_socket, buffer, number_of_bytes_received, 0);
+        if (bytes_sent == -1) {
+            perror("send");
+            break;
+        }
+        total_bytes_received += number_of_bytes_received;
     }
-    //Write the received message in the log file
-    FILE* log_file = fopen("log.txt", "a");
+
+    if (number_of_bytes_received == -1) {
+        perror("recv");
+    }
+
+    // Log and print the total number of bytes delivered successfully
+    FILE *log_file = fopen("log.txt", "a");
     if (log_file == NULL) {
         perror("fopen");
-        close(internet_socket);
+        close(client_internet_socket);
         exit(4);
-
     }
     fprintf(log_file, "------------------------\n");
-    fprintf(log_file, "Message from client: %s\n", buffer);
+    fprintf(log_file, "Total bytes delivered: %d\n", total_bytes_received);
     fprintf(log_file, "------------------------\n");
-    printf( "------------------------\n");
-    printf("Message from client: %s\n", buffer);
-    printf("------------------------\n");
     fclose(log_file);
-
-    // Step 2: Send HTTP GET request
-    http_get(ip_address);
-
+    printf("------------------------\n");
+    printf("Total bytes delivered: %d\n", total_bytes_received);
+    printf("------------------------\n");
 }
 
+/*
 void cleanup( int internet_socket, int client_internet_socket )
 {
     //Step 4.2
@@ -293,3 +315,4 @@ void cleanup( int internet_socket, int client_internet_socket )
     close( client_internet_socket );
     close( internet_socket );
 }
+*/
